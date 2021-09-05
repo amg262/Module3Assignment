68fe8a1d83d3b90d7669fb02561b46bbeae56672
Andrew <andrewmgunn26@gmail.com>
2021-09-05 15:33:32 -0500

    init

--- Module3Assignment/Program.cs
+++ Module3Assignment/Program.cs
@@ -4,6 +4,36 @@ namespace Module3Assignment
 {
     class Program
     {
+        static double Steps(double steps)
+        {
+            double totalPoints = 0;
+            return totalPoints;
+        }
+        
+        static double Yoga(double classes)
+        {
+            double totalPoints = 0;
+            return totalPoints;
+        }
+        
+        static double Aerobics(double classes)
+        {
+            double totalPoints = 0;
+            return totalPoints;
+        }
+        
+        static double Nutrition(double meetings)
+        {
+            double totalPoints = 0;
+            return totalPoints;
+        }
+        
+        static double Steps(double steps)
+        {
+            return 0;
+        }
+        
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
